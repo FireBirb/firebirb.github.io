3c245f01749cdbf10e7c1825a9302ebd86792d73
G7E9K <littlefirebird1@gmail.com>
2025-03-10 13:51:53 +0300

    Initial commit

---
+++ Partials/footer.php
@@ -0,0 +1,22 @@
+<?php
+	echo "
+	<footer class="darkBox">
+		<div>
+			<p>© 2025-2026 ЗАО "Компания"</p>
+			<p>info@name.ru</p>
+		</div>
+			<ul>
+				<li class="clickable"><a href="index.html">Главная</a></li>
+				<li class="clickable"><a href="test.html">Тест</a></li>
+				<li class="clickable"><a href="index.html">Регистрация</a></li>
+				<li class="clickable"><a href="photogallery.html">Галлерея</a></li>
+				<li class="clickable"><a href="attributions.html">Упоминания</a></li>
+			</ul>
+		<div>
+			<div id="SiteLogo"></div>
+			<p><span style="text-decoration: underline; text-underline-offset: 2px;">Разработка сайта</span> -</p>
+			<p>компания "Пиксель плюс"</p>
+		</div>
+	</footer>
+	";
+?>
---
+++ Partials/navigation.php
@@ -0,0 +1,15 @@
+<?php
+	echo "
+	<nav class="darkBox">
+		<p class="clickable"><a href="index.html">Главная</a></p>
+		<p>|</p>
+		<p class="clickable"><a href="test.html">Тест</a></p>
+		<p>|</p>
+		<p class="clickable"><a href="index.html">Регистрация</a></p>
+		<p>|</p>
+		<p class="clickable"><a href="photogallery.html">Галлерея</a></p>
+		<p>|</p>
+		<p class="clickable"><a href="attributions.html">Упоминания</a></p>
+	</nav>
+	";
+?>
